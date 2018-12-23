2e158dbca18d3305469a288a6b1588094f4e8ca3
Hykilpikonna <admin@moecraft.cc>
2018-12-22 22:06:10 -0500

    [+] Create testing node

---
+++ src/main/java/cc/moecraft/livelocation/api/nodes/misc/NodeTest.java
@@ -0,0 +1,28 @@
+package cc.moecraft.livelocation.api.nodes.misc;
+
+import cc.moecraft.livelocation.api.ApiNode;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class NodeTest implements ApiNode
+{
+    @Override
+    public String nodeName()
+    {
+        return "misc.test";
+    }
+
+    @Override
+    public String process(HttpServletRequest request, String content)
+    {
+        return "Success! " + content;
+    }
+}
