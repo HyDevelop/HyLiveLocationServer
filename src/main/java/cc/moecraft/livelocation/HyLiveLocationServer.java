eed10514b0b04947c56d3b2ac4d11d28cd5ef01c
Hykilpikonna <admin@moecraft.cc>
2018-12-22 21:43:35 -0500

    [+] Add Logger and LIM fields

--- src/main/java/cc/moecraft/livelocation/HyLiveLocationServer.java
+++ src/main/java/cc/moecraft/livelocation/HyLiveLocationServer.java
@@ -1,5 +1,10 @@
 package cc.moecraft.livelocation;
 
+import cc.moecraft.logger.HyLogger;
+import cc.moecraft.logger.LoggerInstanceManager;
+import cc.moecraft.logger.environments.ColorSupportLevel;
+import cc.moecraft.logger.environments.ConsoleColoredEnv;
+import cc.moecraft.logger.environments.FileEnv;
 import lombok.Getter;
 import org.eclipse.jetty.server.Server;
 
@@ -15,6 +20,12 @@ import org.eclipse.jetty.server.Server;
 public class HyLiveLocationServer
 {
     private final HLLConfig config;
+
+    private final LoggerInstanceManager lim = new LoggerInstanceManager(
+            new ConsoleColoredEnv(ColorSupportLevel.DEFAULT),
+            new FileEnv("./logs/", "HyLiveLocationServer.log"));
+
+    private final HyLogger logger;
 
     /**
      * 创建一个HyLiveLocationServer对象
