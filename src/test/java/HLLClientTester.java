c9d715b7c381e5af63f38a40cab913a1d18a0d93
Hykilpikonna <admin@moecraft.cc>
2018-12-22 22:08:43 -0500

    [T] Test Api handling system

---
+++ src/test/java/HLLClientTester.java
@@ -0,0 +1,22 @@
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/22 创建!
+ * Created by Hykilpikonna on 2018/12/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class HLLClientTester
+{
+    public static void main(String[] args)
+    {
+        HttpRequest request = HttpUtil.createPost("localhost:19486/api");
+        request.header("node", "misc.test").header("something", "someValue").body("Test request body");
+        HttpResponse response = request.execute();
+        System.out.println(response);
+    }
+}
