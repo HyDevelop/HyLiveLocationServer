155e365a7fa75dcc9a4123489006ba01a80e7ee7
Hykilpikonna <admin@moecraft.cc>
2018-12-22 21:45:34 -0500

    [+] Create ApiNode interface

---
+++ src/main/java/cc/moecraft/livelocation/api/ApiNode.java
@@ -0,0 +1,31 @@
+package cc.moecraft.livelocation.api;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public interface ApiNode
+{
+    /**
+     * Name of the api node.
+     * MUST be all lowercase.
+     *
+     * @return Name of the api node.
+     */
+    public String nodeName();
+
+    /**
+     * Process an api request.
+     *
+     * @param request API request
+     * @param content Content submitted
+     * @return Result.
+     */
+    public String process(HttpServletRequest request, String content);
+}
