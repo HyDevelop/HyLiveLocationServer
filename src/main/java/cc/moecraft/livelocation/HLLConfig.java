fce89877f78d3848282fc5a5c03a710ce3a1148e
Hykilpikonna <admin@moecraft.cc>
2018-12-22 21:41:10 -0500

    [+] Create HLLConfig class

---
+++ src/main/java/cc/moecraft/livelocation/HLLConfig.java
@@ -0,0 +1,16 @@
+package cc.moecraft.livelocation;
+
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/22 创建!
+ * Created by Hykilpikonna on 2018/12/22!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class HLLConfig
+{
+}
