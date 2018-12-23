36f8e89be01601f028324e9f4d14ba0d5e9a634e
Hykilpikonna <admin@moecraft.cc>
2018-12-22 21:40:02 -0500

    [+] Create HyLiveLocationServer class

---
+++ src/main/java/cc/moecraft/livelocation/HyLiveLocationServer.java
@@ -0,0 +1,14 @@
+package cc.moecraft.livelocation;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/22 创建!
+ * Created by Hykilpikonna on 2018/12/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Getter
+public class HyLiveLocationServer
+{
+}
