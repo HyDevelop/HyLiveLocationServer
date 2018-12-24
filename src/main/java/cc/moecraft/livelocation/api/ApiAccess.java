c67125533be70d17f255329becf35dccd72f4ae7
Hykilpikonna <admin@moecraft.cc>
2018-12-24 14:21:22 -0500

    [+] Create ApiAccess class

---
+++ src/main/java/cc/moecraft/livelocation/api/ApiAccess.java
@@ -0,0 +1,24 @@
+package cc.moecraft.livelocation.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/24 创建!
+ * Created by Hykilpikonna on 2018/12/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+@AllArgsConstructor
+public class ApiAccess
+{
+    private final HttpServletRequest request;
+    private final Map<String, String> headers;
+    private final String content;
+}
