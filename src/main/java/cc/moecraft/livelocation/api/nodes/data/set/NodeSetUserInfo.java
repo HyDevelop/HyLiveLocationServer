ceb10b7ea6efd60572fa91b1735f68555eb41cdf
Hykilpikonna <admin@moecraft.cc>
2018-12-24 13:11:36 -0500

    [+] Create set user info api node

---
+++ src/main/java/cc/moecraft/livelocation/api/nodes/data/set/NodeSetUserInfo.java
@@ -0,0 +1,34 @@
+package cc.moecraft.livelocation.api.nodes.data.set;
+
+import cc.moecraft.livelocation.HyLiveLocationServer;
+import cc.moecraft.livelocation.api.HLLApiNode;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/24 创建!
+ * Created by Hykilpikonna on 2018/12/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class NodeSetUserInfo extends HLLApiNode
+{
+    public NodeSetUserInfo(HyLiveLocationServer server)
+    {
+        super(server);
+    }
+
+    @Override
+    public String nodeName()
+    {
+        return "data.set.user-info";
+    }
+
+    @Override
+    public String process(HttpServletRequest request, String content)
+    {
+        return null;
+    }
+}
