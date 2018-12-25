ebee585080258a0cab6403909cd99b6026742025
Hykilpikonna <admin@moecraft.cc>
2018-12-25 12:30:52 -0500

    [+] Construct locationDataset from DataLatest

--- src/main/java/cc/moecraft/livelocation/dataset/LocationDataset.java
+++ src/main/java/cc/moecraft/livelocation/dataset/LocationDataset.java
@@ -1,8 +1,13 @@
 package cc.moecraft.livelocation.dataset;
 
+import cc.moecraft.livelocation.database.model.DataLatest;
+import cc.moecraft.livelocation.database.model.DataLog;
+import cc.moecraft.livelocation.utils.encryption.Encryptor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import static java.lang.Double.parseDouble;
 
 /**
  * 此类由 Hykilpikonna 在 2018/12/24 创建!
@@ -22,4 +27,18 @@ public class LocationDataset
     private final Double longitude; // 经度
     private String submitIp = null;
     private Long submitTime = null;
+
+    /**
+     * 从数据库Model对象创建一个Dataset对象
+     *
+     * @param encryptor 解密工具
+     * @param dataLatest 位置信息
+     */
+    public LocationDataset(Encryptor encryptor, DataLatest dataLatest)
+    {
+        this(dataLatest.getUsername(),
+                parseDouble(encryptor.decrypt(dataLatest.getLatitude())),
+                parseDouble(encryptor.decrypt(dataLatest.getLongitude())),
+                dataLatest.getSubmitIp(), dataLatest.getSubmitTime());
+    }
 }
