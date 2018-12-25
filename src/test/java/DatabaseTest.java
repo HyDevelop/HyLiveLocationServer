dbe522dbf1f695b214dc31f3e09703d9843ee106
Hykilpikonna <admin@moecraft.cc>
2018-12-25 14:34:47 -0500

    [T] Test sql statement

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,20 @@
+import cc.moecraft.livelocation.HyLiveLocationLauncher;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/25 创建!
+ * Created by Hykilpikonna on 2018/12/25!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class DatabaseTest
+{
+    public static void main(String[] args) throws Exception
+    {
+        HyLiveLocationLauncher.main(new String[]{"do-nothing", "-file=./my-secret-config.json"});
+        HyLiveLocationLauncher.getServer().getDatabaseInitializer().initialize();
+
+        System.out.println("");
+    }
+}
