09769c34e95542bc8a9b2334276b5865183d3691
Hykilpikonna <admin@moecraft.cc>
2018-12-25 14:06:08 -0500

    [M] Translate commants to chinese

--- src/test/java/HLLApiClientTester.java
+++ src/test/java/HLLApiClientTester.java
@@ -15,26 +15,27 @@ public class HLLApiClientTester
     public static void main(String[] args)
     {
         HLLApiClient client = new HLLApiClient("localhost:19486/api", new Encryptor("default-pw"));
-        System.out.println("\n-------------------=[ Send Test Request ]=--------------------");
+
+        System.out.println("\n-------------------=[ 测试请求 ]=--------------------");
         System.out.println(client.send("misc.test", "Test request body", "something", "someValue"));
 
-        System.out.println("\n---------------=[ Update TestUser Location ]=-----------------");
+        System.out.println("\n-------------------=[ 更新位置 ]=--------------------");
         System.out.println(client.sendSetLocationDataset(new LocationDataset("testUser", 47d, -12d)));
         // System.out.println(client.sendSetLocationDataset(new LocationDataset("inactiveUser", 45d, -12d)));
 
-        System.out.println("\n---------------------=[ Get User List ]=----------------------");
+        System.out.println("\n-----------------=[ 获取用户列表 ]=------------------");
         System.out.println(client.getUserListAll());
 
-        System.out.println("\n------------------=[ Get User List Active ]=------------------");
+        System.out.println("\n---------------=[ 获取活跃用户列表 ]=----------------");
         System.out.println(client.getUserListActive());
 
-        System.out.println("\n------------------=[ Get User List Inactive ]=------------------");
+        System.out.println("\n--------------=[ 获取不活跃用户列表 ]=---------------");
         System.out.println(client.getUserListInactive());
 
-        System.out.println("\n------------------=[ Get User Location ]=------------------");
+        System.out.println("\n-----------------=[ 获取用户位置 ]=------------------");
         System.out.println(client.getUserLocation("testUser"));
 
-        System.out.println("\n------------------=[ Get User Location History ]=------------------");
+        System.out.println("\n-----------------=[ 获取历史位置 ]=------------------");
         System.out.println(client.getUserLocationHistory("testUser"));
     }
 }
