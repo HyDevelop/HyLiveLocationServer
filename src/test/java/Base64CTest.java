b865073e9c4a67a1b4d65f686e32d0ae0f24f1ef
Hykilpikonna <admin@moecraft.cc>
2018-12-24 20:21:20 -0500

    [T] Test base64C

---
+++ src/test/java/Base64CTest.java
@@ -0,0 +1,21 @@
+import cc.moecraft.livelocation.utils.encryption.Base64C;
+import cc.moecraft.livelocation.utils.encryption.Encryptor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/24 创建!
+ * Created by Hykilpikonna on 2018/12/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class Base64CTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Base64C.encodeBase64C("Hello world !"));
+        System.out.println(Base64C.decodeBase64CStr("SGVsbG8gd29ybGQgIQ"));
+        System.out.println(Base64C.decodeBase64CStr("SGVsbG8gd29ybGQgIQ=="));
+        System.out.println(new Encryptor("default-pw").decrypt("E4CtWYzemXCPRULSUUDdcHrreGttNCR20b3++pAMoXXBvd/G4h1SScZX9AsXj7P/GHOh6UhFc2Kj2uTxLkkdhQ=="));
+    }
+}
