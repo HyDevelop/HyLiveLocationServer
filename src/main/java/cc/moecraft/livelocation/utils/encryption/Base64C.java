5e319d01024e81ef91ca3cbca18626613f49b3ed
Hykilpikonna <admin@moecraft.cc>
2018-12-24 20:19:42 -0500

    [+] Custom Base64 encoder

---
+++ src/main/java/cc/moecraft/livelocation/utils/encryption/Base64C.java
@@ -0,0 +1,51 @@
+package cc.moecraft.livelocation.utils.encryption;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.IOException;
+
+import static cc.moecraft.utils.StringUtils.repeat;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/24 创建!
+ * Created by Hykilpikonna on 2018/12/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class Base64C
+{
+    public static String encodeBase64C(byte[] bytes)
+    {
+        return new BASE64Encoder().encode(bytes)
+                .replace("=", "")
+                .replace("+", "-")
+                .replace("/", "_")
+    }
+
+    public static String encodeBase64C(String text)
+    {
+        return encodeBase64C(text.getBytes());
+    }
+
+    public static byte[] decodeBase64C(String text)
+    {
+        try
+        {
+            return new BASE64Decoder().decodeBuffer(text
+                    .replace("-", "+")
+                    .replace("_", "/"));
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String decodeBase64CStr(String text)
+    {
+        return new String(decodeBase64C(text));
+    }
+}
