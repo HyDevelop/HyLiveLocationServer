18ac4d7881d24f4967b9c8da0570ee6e631e4ee7
Hykilpikonna <admin@moecraft.cc>
2018-12-24 20:52:25 -0500

    [+] Create logging disabler

---
+++ src/main/java/cc/moecraft/livelocation/utils/MiscUtils.java
@@ -0,0 +1,20 @@
+package cc.moecraft.livelocation.utils;
+
+import org.eclipse.jetty.util.log.Logger;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/24 创建!
+ * Created by Hykilpikonna on 2018/12/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class MiscUtils
+{
+    public static void disableLogging()
+    {
+        System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+        System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "OFF");
+    }
+}
