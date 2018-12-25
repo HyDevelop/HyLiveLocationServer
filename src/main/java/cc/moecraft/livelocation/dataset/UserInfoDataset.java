7fbf18837780a870230e71cc6ef823b8bfa231a2
Hykilpikonna <admin@moecraft.cc>
2018-12-25 08:20:47 -0500

    [+] Create constructor to construct UserInfoDataset directly from UserInfo

--- src/main/java/cc/moecraft/livelocation/dataset/UserInfoDataset.java
+++ src/main/java/cc/moecraft/livelocation/dataset/UserInfoDataset.java
@@ -1,5 +1,6 @@
 package cc.moecraft.livelocation.dataset;
 
+import cc.moecraft.livelocation.database.model.UserInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -18,4 +19,14 @@ public class UserInfoDataset
     private final String username;
     private final String avatarUrl;
     private Long lastActive = null;
+
+    /**
+     * 从数据库Model对象创建一个Dataset对象
+     *
+     * @param userInfo 用户信息
+     */
+    public UserInfoDataset(UserInfo userInfo)
+    {
+        this(userInfo.getUsername(), userInfo.getAvatarUrl(), userInfo.getLastActive());
+    }
 }
