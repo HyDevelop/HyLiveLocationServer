ed0aed3b5ce8e66b12191d45b52850c7884581d7
Hykilpikonna <admin@moecraft.cc>
2018-12-24 20:52:40 -0500

    [+] Disable jetty logging

--- src/main/java/cc/moecraft/livelocation/utils/MiscUtils.java
+++ src/main/java/cc/moecraft/livelocation/utils/MiscUtils.java
@@ -16,5 +16,28 @@ public class MiscUtils
     {
         System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
         System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "OFF");
+        System.setProperty("org.eclipse.jetty.LEVEL", "OFF");
+
+        org.eclipse.jetty.util.log.Log.setLog(new NoLogging());
     }
+
+    public static class NoLogging implements Logger
+    {
+        @Override public String getName() { return "false"; }
+        @Override public void warn(String msg, Object... args) { }
+        @Override public void warn(Throwable thrown) { thrown.printStackTrace(); }
+        @Override public void warn(String msg, Throwable thrown) { System.err.println(msg); thrown.printStackTrace(); }
+        @Override public void info(String msg, Object... args) { }
+        @Override public void info(Throwable thrown) { }
+        @Override public void info(String msg, Throwable thrown) { }
+        @Override public boolean isDebugEnabled() { return false; }
+        @Override public void setDebugEnabled(boolean enabled) { }
+        @Override public void debug(String msg, Object... args) { }
+        @Override public void debug(String msg, long value) { }
+        @Override public void debug(Throwable thrown) { }
+        @Override public void debug(String msg, Throwable thrown) { }
+        @Override public Logger getLogger(String name) { return this; }
+        @Override public void ignore(Throwable ignored) { }
+    }
+
 }
