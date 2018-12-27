a0fd499425ac141e00f33dac206a3e707bf65828
Hykilpikonna <admin@moecraft.cc>
2018-12-26 20:56:36 -0500

    [T] Test get server cofnig

--- src/test/java/HLLApiClientTester.java
+++ src/test/java/HLLApiClientTester.java
@@ -16,15 +16,13 @@ public class HLLApiClientTester
     {
         HLLApiClient client = new HLLApiClient("localhost:19486/api", new Encryptor("default-pw"));
 
-        System.out.println("\n-------------------=[ 测试请求 ]=--------------------");
-        System.out.println(client.send("misc.test", "Test request body", "something", "someValue"));
+        System.out.println("");
 
-        System.out.println("\n-------------------=[ 更新位置 ]=--------------------");
-        System.out.println(client.sendSetLocationDataset(new LocationDataset("testUser", 47d, -12d)));
-        // System.out.println(client.sendSetLocationDataset(new LocationDataset("inactiveUser", 45d, -12d)));
+        System.out.println("\n-------------------=[ 上传位置 ]=--------------------");
+        System.out.println(client.sendSetLocationDataset(new LocationDataset("testUser", 47d, -12d))); // 返回 Success
 
         System.out.println("\n-----------------=[ 获取用户列表 ]=------------------");
-        System.out.println(client.getUserListAll());
+        System.out.println(client.getUserListAll()); // 返回 ArrayList<UserInfoDataset>
 
         System.out.println("\n---------------=[ 获取活跃用户列表 ]=----------------");
         System.out.println(client.getUserListActive());
@@ -33,9 +31,12 @@ public class HLLApiClientTester
         System.out.println(client.getUserListInactive());
 
         System.out.println("\n-----------------=[ 获取用户位置 ]=------------------");
-        System.out.println(client.getUserLocation("testUser"));
+        System.out.println(client.getUserLocation("testUser")); // 返回 LocationDataset
 
         System.out.println("\n-----------------=[ 获取历史位置 ]=------------------");
-        System.out.println(client.getUserLocationHistory("testUser"));
+        System.out.println(client.getUserLocationHistory("testUser")); // 返回 ArrayList<LocationDataset>
+
+        System.out.println("\n-----------------=[ 获取配置文件 ]=------------------");
+        System.out.println(client.getServerConfig()); // 返回 HLLConfig
     }
 }
